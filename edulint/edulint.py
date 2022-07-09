de2618af29ec5016ac1c12e5c6b2423c7df4219f
Anna Rechtackova <xrechtac@fi.muni.cz>
2022-07-09 12:53:48 +0200

    lint: add problem setters

--- edulint/edulint.py
+++ edulint/edulint.py
@@ -27,6 +27,38 @@ class Problem:
     end_line: Optional[int] = None
     end_column: Optional[int] = None
 
+    def set_source(self, v: str) -> "Problem":
+        self.source = v
+        return self
+
+    def set_path(self, v: str) -> "Problem":
+        self.path = v
+        return self
+
+    def set_line(self, v: int) -> "Problem":
+        self.line = v
+        return self
+
+    def set_column(self, v: int) -> "Problem":
+        self.column = v
+        return self
+
+    def set_code(self, v: str) -> "Problem":
+        self.code = v
+        return self
+
+    def set_text(self, v: str) -> "Problem":
+        self.text = v
+        return self
+
+    def set_end_line(self, v: Optional[int]) -> "Problem":
+        self.end_line = v
+        return self
+
+    def set_end_column(self, v: Optional[int]) -> "Problem":
+        self.end_column = v
+        return self
+
     def __str__(self):
         return f"{self.path}:{self.line}:{self.column}: " \
                f"{self.code} {self.text}"
