0d347b5cf62ad846abea39ae6723cdaae5b5ab10
Anna Rechtackova <xrechtac@fi.muni.cz>
2022-08-05 22:41:44 +0200

    docs: tables wrap

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,6 +57,10 @@ html_theme_options = {
 }
 
 
+def setup(app):
+    app.add_css_file("theme_overrides.css")
+
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
