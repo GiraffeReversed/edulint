ef19b061c8a5b3524193893c6def99a56f713f60
Anna Rechtackova <xrechtac@fi.muni.cz>
2023-02-01 14:40:10 +0100

    checkers: more improper loop checks

--- edulint/linting/checkers/basic_checker.py
+++ edulint/linting/checkers/basic_checker.py
@@ -1,5 +1,5 @@
 from astroid import nodes  # type: ignore
-from typing import TYPE_CHECKING, Optional, List, Tuple, Union, TypeVar, Iterator, Any, Dict, Callable
+from typing import TYPE_CHECKING, Optional, List, Tuple, Union, TypeVar, Iterator, Any, Dict, Set, Callable
 import re
 
 from pylint.checkers import BaseChecker  # type: ignore
@@ -508,10 +508,30 @@ class ImproperLoop(BaseChecker):
             "Use tighter range boundaries, the %s iteration never happens.",
             "use-tighter-boundaries",
             "Emitted when the boundaries of a range can be made tighter."
+        ),
+        "R6303": (
+            "Iterated structure %s is being modified inside the for loop body. Use while loop or iterate over a copy.",
+            "modifying-iterated-structure",
+            "Emitted when the structure that is being iterated over is being modified in the for loops body."
+        ),
+        "R6304": (
+            "Changing the control variable %s of a for loop has no effect.",
+            "changing-control-variable",
+            "Emitted when the control variable of a for loop is being changed."
+        ),
+        "R6305": (
+            "Use for loop.",
+            "use-for-loop",
+            "Emitted when a while loop can be transformed into a for loop."
+        ),
+        "R6306": (
+            "Inner for loop shadows outer for loop's control variable %s.",
+            "loop-shadows-control-variable",
+            "Emitted when a for loop shadows control variable of an outer for loop."
         )
     }
 
-    def visit_while(self, node: nodes.While) -> None:
+    def _check_no_while_true(self, node: nodes.While) -> None:
         if not isinstance(node.test, nodes.Const) or not node.test.bool_value():
             return
 
@@ -519,7 +539,75 @@ class ImproperLoop(BaseChecker):
         if isinstance(first, nodes.If) and isinstance(first.body[-1], nodes.Break):
             self.add_message("no-while-true", node=node, args=(first.test.as_string()))
 
-    def visit_for(self, node: nodes.For) -> None:
+    def _check_use_for_loop(self, node: nodes.While) -> None:
+        def get_relevant_vals(node: nodes.NodeNG, result: Set[nodes.NodeNG] = None) -> Tuple[bool, Set[nodes.NodeNG]]:
+            result = result if result is not None else set()
+
+            if isinstance(node, nodes.Const):
+                return True, result
+            if isinstance(node, nodes.Name):
+                result.add(node)
+                return True, result
+            if isinstance(node, nodes.BinOp):
+                v1 = get_relevant_vals(node.left, result)
+                v2 = get_relevant_vals(node.right, result)
+                return v1 and v2, result
+            if isinstance(node, nodes.Call) and node.func.as_string() == "len" and len(node.args) == 1:
+                return get_relevant_vals(node.args[0], result)
+            return False, result
+
+        # TODO allow different increments?
+        def adds_or_subtracts_one(node: nodes.NodeNG) -> bool:
+            if not isinstance(node, nodes.AssignName):
+                return False
+
+            expr = node.parent
+            if isinstance(expr, nodes.AugAssign):
+                return expr.op in ("+=", "-=") and get_const_value(expr.value) == 1
+            if isinstance(expr, nodes.Assign) and isinstance(expr.value, nodes.BinOp):
+                binop = expr.value
+                return binop.op in ("+", "-") \
+                    and ((binop.left.as_string() == node.as_string() and get_const_value(binop.right) == 1)
+                         or (binop.right.as_string() == node.as_string() and get_const_value(binop.left) == 1))
+            return False
+
+        test = node.test
+        if not isinstance(test, nodes.Compare) or len(test.ops) != 1 or test.ops[0][0] not in ("<", "<=", ">", ">="):
+            return
+
+        lt, rt = test.left, test.ops[0][1]
+        lt_decomposable, lt_vals = get_relevant_vals(lt)
+        rt_decomposable, rt_vals = get_relevant_vals(rt)
+        if not lt_decomposable or not rt_decomposable:
+            return
+
+        all_vals = lt_vals | rt_vals
+        listener = ModifiedListener(all_vals)
+        listener.visit_many(node.body)
+
+        all_modifiers = [(val in lt_vals, listener.get_all_modifiers(val)) for val in all_vals]
+        nonempty_modifiers = [(from_lt, modifiers) for (from_lt, modifiers) in all_modifiers if len(modifiers) > 0]
+
+        if len(nonempty_modifiers) != 1:
+            return
+
+        from_lt, only_modifiers = nonempty_modifiers[0]
+        if len(only_modifiers) != 1:
+            return
+
+        only_modifier = only_modifiers[0]
+
+        if self._get_block_line(only_modifier).parent != node or not adds_or_subtracts_one(only_modifier) \
+                or (only_modifier.as_string() != lt.as_string() and only_modifier.as_string() != rt.as_string()):
+            return
+
+        self.add_message("use-for-loop", node=node)
+
+    def visit_while(self, node: nodes.While) -> None:
+        self._check_no_while_true(node)
+        self._check_use_for_loop(node)
+
+    def _check_use_tighter_bounds(self, node: nodes.For) -> None:
         def compares_equality(node: nodes.NodeNG) -> bool:
             return isinstance(node, nodes.Compare) and len(node.ops) == 1 and node.ops[0][0] == "=="
 
@@ -571,6 +659,68 @@ class ImproperLoop(BaseChecker):
                     self.add_message("use-tighter-boundaries", node=node, args=("last",))
             # TODO expand, tips: len(node.body) == 1, isinstance(if_.body[-1], nodes.Return), allow nodes in the middle,
             #                    allow longer bodies
+
+    @staticmethod
+    def _get_block_line(node: nodes.NodeNG):
+        while not isinstance(node, nodes.Statement):
+            node = node.parent
+        return node
+
+    @staticmethod
+    def _get_last_block_line(node: nodes.NodeNG):
+        node = ImproperLoop._get_block_line(node)
+
+        while node.next_sibling() is not None:
+            node = node.next_sibling()
+        return node
+
+    def _check_modifying_iterable(self, node: nodes.For) -> None:
+        iterated = node.iter
+        if type(iterated) not in (nodes.Name, nodes.Attribute):  # TODO allow any node type
+            return
+
+        listener = ModifiedListener({iterated})
+        listener.visit_many(node.body)
+
+        for modifier in listener.get_sure_modifiers(iterated):
+            if isinstance(modifier, nodes.Call) \
+                    and type(self._get_last_block_line(modifier)) not in (nodes.Break, nodes.Return):
+                self.add_message("modifying-iterated-structure", node=modifier, args=(get_name(iterated),))
+
+    def _check_control_variable_changes(self, node: nodes.For) -> None:
+        def is_last_block(node: nodes.NodeNG, for_: nodes.For) -> bool:
+            stmt = self._get_block_line(node)
+            while not isinstance(node, nodes.Module):
+                if type(stmt) in (nodes.For, nodes.While):
+                    return False
+                if stmt == for_.body[-1]:
+                    return True
+                last_block_stmt = self._get_last_block_line(stmt)
+                if stmt != last_block_stmt:
+                    return False
+                stmt = last_block_stmt.parent
+            return False
+
+        range_params = get_range_params(node.iter)
+        if range_params is None:
+            return
+
+        control_var = node.target
+        listener = ModifiedListener({control_var})
+        listener.visit_many(node.body)
+
+        for modifier in listener.get_all_modifiers(control_var):
+            mod_statement = self._get_block_line(modifier)
+            if isinstance(mod_statement, nodes.For) and mod_statement.target.as_string() == control_var.as_string() \
+                    and control_var.as_string() != "_":
+                self.add_message("loop-shadows-control-variable", node=mod_statement, args=(modifier.as_string()))
+            if is_last_block(mod_statement, node):
+                self.add_message("changing-control-variable", node=mod_statement, args=(control_var.as_string(),))
+
+    def visit_for(self, node: nodes.For) -> None:
+        self._check_use_tighter_bounds(node)
+        self._check_modifying_iterable(node)
+        self._check_control_variable_changes(node)
 
 
 class NoGlobalVars(BaseChecker):
--- tests/test_lint.py
+++ tests/test_lint.py
@@ -803,6 +803,45 @@ class TestImproperLoop:
             [p.set_code("R6301") for p in expected_output]
         )
 
+    @pytest.mark.parametrize("lines,expected_output", [
+        ([
+            "n = 10",
+            "i = 0",
+            "while i < n:",
+            "    print('foo' + str(i))",
+            "    i += 1",
+        ], [
+            lazy_problem().set_line(3)
+            .set_text("Use for loop.")
+        ]),
+    ])
+    def test_use_for_loop_custom(self, lines: List[str], expected_output: List[Problem]) -> None:
+        create_apply_and_lint(
+            lines,
+            [Arg(Option.PYLINT, "--disable=all"),
+             Arg(Option.PYLINT, "--enable=use-for-loop"),
+             Arg(Option.NO_FLAKE8, "on")],
+            [p.set_code("R6305") for p in expected_output]
+        )
+
+    @pytest.mark.parametrize("lines,expected_output", [
+        ([
+            "for i in range(10):",
+            "    print('foo')",
+            "    i += 1"
+        ], [
+            lazy_problem().set_line(3).set_text("Changing the control variable i of a for loop has no effect.")
+        ]),
+    ])
+    def test_changing_control_variable_custom(self, lines: List[str], expected_output: List[Problem]) -> None:
+        create_apply_and_lint(
+            lines,
+            [Arg(Option.PYLINT, "--disable=all"),
+             Arg(Option.PYLINT, "--enable=changing-control-variable"),
+             Arg(Option.NO_FLAKE8, "on")],
+            [p.set_code("R6304") for p in expected_output]
+        )
+
     @pytest.mark.parametrize("filename,expected_output", [
         ("hw14062.py", [
             lazy_problem().set_code("R6302").set_line(29)
@@ -822,6 +861,104 @@ class TestImproperLoop:
             filename,
             [Arg(Option.PYLINT, "--disable=all"), Arg(Option.PYLINT, "--enable=use-tighter-boundaries")],
             expected_output
+        )
+
+    @pytest.mark.parametrize("filename,expected_output", [
+        ("010666-even.py", [lazy_problem().set_line(9)]),
+        ("012630-next.py", []),
+        ("012986-nested.py", [lazy_problem().set_line(35)]),
+        ("013450-next.py", []),
+        ("014286-coins.py", []),
+        ("014556-prime.py", [lazy_problem().set_line(13).set_text("Use for loop.")]),
+        ("014962-sequence.py", [lazy_problem().set_line(18).set_text("Use for loop.")]),
+        ("015794-amicable.py", [lazy_problem().set_line(14)]),
+        ("016119-coins.py", [lazy_problem().set_line(22)]),
+        ("017667-coins.py", []),
+        ("023140-credit.py", []),
+        ("043611-swap_columns.py", []),
+    ])
+    def test_use_for_loop_files(self, filename: str, expected_output: List[Problem]) -> None:
+        apply_and_lint(
+            filename,
+            [Arg(Option.PYLINT, "--disable=all"), Arg(Option.PYLINT, "--enable=use-for-loop")],
+            [problem.set_code("R6305") for problem in expected_output]
+        )
+
+    @pytest.mark.parametrize("filename,expected_output", [
+        ("034636-right_cellular.py", []),
+        ("104526-subset_sum.py", [
+            lazy_problem().set_line(17)
+            .set_text("Iterated structure nums is being modified inside the for loop body. "
+                      "Use while loop or iterate over a copy."),
+            lazy_problem().set_line(22)
+        ]),
+        ("035794-partition.py", []),
+        ("hw24328.py", [
+            lazy_problem().set_line(52)
+            .set_text("Iterated structure row is being modified inside the for loop body. "
+                      "Use while loop or iterate over a copy."),
+            lazy_problem().set_line(53),
+            lazy_problem().set_line(56),
+            lazy_problem().set_line(99),
+            lazy_problem().set_line(100),
+            lazy_problem().set_line(103),
+        ]),
+        ("hw44081.py", [
+            lazy_problem().set_line(149)
+            .set_text("Iterated structure traders is being modified inside the for loop body. "
+                      "Use while loop or iterate over a copy.")
+        ]),
+    ])
+    def test_modifying_iterated_files(self, filename: str, expected_output: List[Problem]) -> None:
+        apply_and_lint(
+            filename,
+            [Arg(Option.PYLINT, "--disable=all"), Arg(Option.PYLINT, "--enable=modifying-iterated-structure")],
+            [problem.set_code("R6303") for problem in expected_output]
+        )
+
+    @pytest.mark.parametrize("filename,expected_output", [
+        ("hw33176.py", []),
+        ("m4521.py", [
+            lazy_problem().set_line(117).set_text("Inner for loop shadows outer for loop's control variable i.")
+        ])
+    ])
+    def test_shadowed_control_var_files(self, filename: str, expected_output: List[Problem]) -> None:
+        apply_and_lint(
+            filename,
+            [Arg(Option.PYLINT, "--disable=all"), Arg(Option.PYLINT, "--enable=loop-shadows-control-variable"),
+             Arg(Option.NO_FLAKE8, "on")],
+            expected_output
+        )
+
+    @pytest.mark.parametrize("filename,expected_output", [
+        ("014659-next.py", [
+            lazy_problem().set_line(24).set_text("Changing the control variable i of a for loop has no effect.")
+        ]),
+        ("hw24226.py", [
+            lazy_problem().set_line(77).set_text("Changing the control variable j of a for loop has no effect."),
+            lazy_problem().set_line(101).set_text("Changing the control variable p of a for loop has no effect."),
+        ]),
+        ("hw24387.py", [
+            lazy_problem().set_line(98).set_text("Changing the control variable i of a for loop has no effect."),
+            lazy_problem().set_line(129),
+        ]),
+        ("hw34406.py", []),
+        ("hw34666.py", [
+            lazy_problem().set_line(40),
+            lazy_problem().set_line(43),
+            lazy_problem().set_line(51),
+            lazy_problem().set_line(54),
+            lazy_problem().set_line(57),
+            lazy_problem().set_line(71),
+            lazy_problem().set_line(74),
+            lazy_problem().set_line(77),
+        ]),
+    ])
+    def test_changing_control_var_files(self, filename: str, expected_output: List[Problem]) -> None:
+        apply_and_lint(
+            filename,
+            [Arg(Option.PYLINT, "--disable=all"), Arg(Option.PYLINT, "--enable=changing-control-variable")],
+            [problem.set_code("R6304") for problem in expected_output]
         )
 
 
