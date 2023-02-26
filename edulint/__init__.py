56c94e07611c7e8650a77f554b46115a44eeaa53
Anna Rechtackova <xrechtac@fi.muni.cz>
2023-02-27 00:52:44 +0100

    package: inc version

--- edulint/__init__.py
+++ edulint/__init__.py
@@ -5,5 +5,5 @@ from .linting.linting import lint_one, lint_many
 
 __all__ = ["Linter", "Config", "Problem", "lint_one", "lint_many"]
 
-__version__ = "2.1.4"
+__version__ = "2.2.0"
 __version_info__ = tuple(map(int, __version__.split(".")))
