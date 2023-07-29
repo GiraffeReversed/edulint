c34a40c3a3d8d12016a16824534edbc2aef60906
Anna Rechtackova <xrechtac@fi.muni.cz>
2023-07-29 13:42:37 +0200

    main: allow linting directories

--- edulint/edulint.py
+++ edulint/edulint.py
@@ -1,7 +1,7 @@
 from edulint.config.config import Config, get_config, get_cmd_args
 from edulint.linting.problem import Problem
 from edulint.linting.linting import lint_many, sort
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 import argparse
 import os
 import sys
@@ -10,10 +10,25 @@ import sys
 def setup_argparse() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Lints provided code.")
     parser.add_argument("-o", "--option", dest="options", default=[], action="append", help="possible option")
-    parser.add_argument("files", metavar="FILE", nargs="+", help="the file(s) to lint")
+    parser.add_argument(
+        "files_or_dirs", metavar="FILE-OR-DIRECTORY", nargs="+", help="the file(s) or directory(ies) to lint"
+    )
     parser.add_argument("--json", action="store_true",
                         help="should output problems in json format")
     return parser.parse_args()
+
+
+def extract_files(files_or_dirs: List[str], result: List[str] = None) -> List[str]:
+    def extract_files_rec(prefix: Optional[str], files_or_dirs: List[str], result: List[str]) -> List[str]:
+        for file_or_dir in files_or_dirs:
+            full_path = os.path.join(prefix, file_or_dir) if prefix is not None else file_or_dir
+            if os.path.isdir(full_path):
+                extract_files_rec(full_path, os.listdir(full_path), result)
+            elif os.path.splitext(full_path)[1] == ".py":
+                result.append(full_path)
+        return result
+
+    return extract_files_rec(None, files_or_dirs, [])
 
 
 def partition(files: List[str], configs: List[Config]) -> List[Tuple[List[str], Config]]:
@@ -31,10 +46,11 @@ def main() -> int:
     args = setup_argparse()
     cmd_args = get_cmd_args(args)
 
-    configs = [get_config(filename, cmd_args=cmd_args) for filename in args.files]
+    files = extract_files(args.files_or_dirs)
+    configs = [get_config(filename, cmd_args=cmd_args) for filename in files]
 
     try:
-        result = sort(args.files, lint_many(partition(args.files, configs)))
+        result = sort(files, lint_many(partition(files, configs)))
     except TimeoutError as e:
         print(e, file=sys.stderr)
         sys.exit(1)
@@ -44,7 +60,7 @@ def main() -> int:
     else:
         prev_problem = None
         for problem in result:
-            if len(args.files) > 1 and (prev_problem is None or prev_problem.path != problem.path):
+            if len(files) > 1 and (prev_problem is None or prev_problem.path != problem.path):
                 print(f"****************** {os.path.basename(problem.path)}")
                 prev_problem = problem
 
