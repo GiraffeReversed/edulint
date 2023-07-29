fa18c3406ab8ad3bdb23a02898168d5fb831973c
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2023-07-29 12:35:04 +0200

    package: expose function get_explanations

--- edulint/__init__.py
+++ edulint/__init__.py
@@ -2,8 +2,9 @@ from .linters import Linter
 from .config.config import Config
 from .linting.problem import Problem
 from .linting.linting import lint_one, lint_many
+from .explanations import get_explanations
 
-__all__ = ["Linter", "Config", "Problem", "lint_one", "lint_many"]
+__all__ = ["Linter", "Config", "Problem", "lint_one", "lint_many", "get_explanations"]
 
-__version__ = "2.6.4"
+__version__ = "2.6.5"
 __version_info__ = tuple(map(int, __version__.split(".")))
