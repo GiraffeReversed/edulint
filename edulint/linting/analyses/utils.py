2a75625be1defa290e4a490d7d8fa9b79b9e7457
danielczinege <danczinege@gmail.com>
2025-02-14 19:58:56 +0100

    fix: _may_contain_global_variable_modified_in_function()

--- edulint/linting/analyses/utils.py
+++ edulint/linting/analyses/utils.py
@@ -1,10 +1,10 @@
-from typing import Union, List
+from typing import Union, List, Set, Optional
 from astroid import nodes
 
 from edulint.linting.analyses.types import guess_type, Type
 from edulint.linting.analyses.data_dependency import vars_in, modified_in, get_events_in
 from edulint.linting.checkers.utils import has_more_assign_targets, is_chained_assignment
-from edulint.linting.analyses.var_events import VarEventType
+from edulint.linting.analyses.var_events import VarEventType, VarEvent
 
 IMMUTABLE_TYPES = [Type.BOOL, Type.FLOAT, Type.INT, Type.STRING, Type.TUPLE]
 
@@ -46,23 +46,58 @@ def may_contain_mutable_var(node: nodes.NodeNG) -> bool:
     return False
 
 
+def _get_body_of_called_function(definition: VarEvent) -> Optional[List[nodes.NodeNG]]:
+    if not isinstance(definition.node.parent, (nodes.AnnAssign, nodes.Assign, nodes.FunctionDef)):
+        return None
+
+    if isinstance(definition.node, nodes.FunctionDef):
+        return definition.node.body
+
+    if (
+        is_chained_assignment(definition.node.parent)
+        or has_more_assign_targets(definition.node.parent)
+        or not isinstance(definition.node.parent.value, nodes.Lambda)
+    ):
+        return None
+
+    return [definition.node.parent.value]
+
+
 def _may_contain_global_variable_modified_in_function(
-    node: nodes.NodeNG, nodes_: List[nodes.NodeNG]
+    node: nodes.NodeNG, nodes_: List[nodes.NodeNG], checked_function_names: Set[str] = set()
 ) -> bool:
     for event in get_events_in(nodes_, [VarEventType.READ]):
         if not isinstance(event.node.parent, nodes.Call) or event.node != event.node.parent.func:
             continue
+
         for definition in event.definitions:
-            if vars_from_node_may_be_modified_in(node, definition.node.body):
+            func_name = event.node.name
+            if func_name in checked_function_names:
+                continue
+
+            checked_function_names.add(func_name)
+            body = _get_body_of_called_function(definition)
+
+            if (
+                body is None
+                or vars_from_node_may_be_directly_modified_in(node, body)
+                or _may_contain_global_variable_modified_in_function(
+                    node, body, checked_function_names
+                )
+            ):
                 return True
+
     return False
 
 
+def vars_from_node_may_be_directly_modified_in(
+    node: nodes.NodeNG, nodes_: List[nodes.NodeNG]
+) -> bool:
+    return may_contain_mutable_var(node) or modified_in(list(vars_in(node).keys()), nodes_)
+
+
 def vars_from_node_may_be_modified_in(node: nodes.NodeNG, nodes_: List[nodes.NodeNG]) -> bool:
-    """If returns `False`, then vars from `node` are not modified in `nodes` for sure.
-    Note: may not work when there are some global variables in `node`."""
-    return (
-        may_contain_mutable_var(node)
-        or modified_in(list(vars_in(node).keys()), nodes_)
-        or _may_contain_global_variable_modified_in_function(node, nodes_)
-    )
+    """If returns `False`, then vars from `node` are not modified in `nodes` for sure."""
+    return vars_from_node_may_be_directly_modified_in(
+        node, nodes_
+    ) or _may_contain_global_variable_modified_in_function(node, nodes_)
